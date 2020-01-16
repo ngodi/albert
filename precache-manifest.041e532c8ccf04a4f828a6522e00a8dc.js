019b722f7ee9f299345de8a185bedc888840b901
ngodi <albertngodi@gmail.com>
2020-01-16 01:38:56 +0100

    Updates

---
+++ precache-manifest.041e532c8ccf04a4f828a6522e00a8dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176206fa90dd8ebc87fdaaced5a3d218",
+    "url": "/albert/index.html"
+  },
+  {
+    "revision": "675f1237e9e73a15dd9b",
+    "url": "/albert/static/css/main.ec2dd13a.chunk.css"
+  },
+  {
+    "revision": "66731a2a63278df33433",
+    "url": "/albert/static/js/2.e14c0352.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/albert/static/js/2.e14c0352.chunk.js.LICENSE"
+  },
+  {
+    "revision": "675f1237e9e73a15dd9b",
+    "url": "/albert/static/js/main.f6ff9d4c.chunk.js"
+  },
+  {
+    "revision": "95ec2ce6856185f0043d",
+    "url": "/albert/static/js/runtime-main.27aaa51f.js"
+  }
+]);
