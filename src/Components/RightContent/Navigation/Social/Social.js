a902165b75a652c1d04cd51726c1d4a41205d040
ngodi <albertngodi@gmail.com>
2020-01-28 01:16:48 +0100

    fixed github link

--- src/Components/RightContent/Navigation/Social/Social.js
+++ src/Components/RightContent/Navigation/Social/Social.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import './Social.css';
 
@@ -6,9 +7,9 @@ const Social = () => {
     <div class="social">
         <h4>Let's connect</h4>
         <div class="social-links">
-        <a href='www.github.com/ngodi'><i class="fab fa-2x fa-github"></i></a>
-        <a href='https://www.linkedin.com/in/albert-ngodi-b80267174/'><i class="fab fa-2x fa-linkedin-in"></i></a>
-        <a href='https://twitter.com/albertngodi'> <i class="fab fa-2x fa-twitter"></i></a>
+        <a target="_blank" href='https://github.com/ngodi'><i class="fab fa-2x fa-github"></i></a>
+        <a target="_blank" href='https://www.linkedin.com/in/albert-ngodi-b80267174/'><i class="fab fa-2x fa-linkedin-in"></i></a>
+        <a target="_blank" href='https://twitter.com/albertngodi'> <i class="fab fa-2x fa-twitter"></i></a>
       
         </div>
     </div>
