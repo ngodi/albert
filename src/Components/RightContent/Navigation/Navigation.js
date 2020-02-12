43d401765894d5f7aaa990b6a42fb235a80f4e6a
ngodi <albertngodi@gmail.com>
2020-02-12 01:45:12 +0100

    header section updated with background picture

--- src/Components/RightContent/Navigation/Navigation.js
+++ src/Components/RightContent/Navigation/Navigation.js
@@ -6,11 +6,12 @@ import Heading from './Heading/Heading';
 const Navigation = () => {
   return (
     <div class="navigation" >
+      
         <nav>
         <a href="#about">About Me</a>
         <a href="#projects">Projects</a>
-        <a href="#education">Education</a>
         <a href="#experience">Experience</a>
+        <a href="#education">Education</a>
         </nav>
       <Heading />
     
