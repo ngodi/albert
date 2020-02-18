97349a01b51291d96428178eb4751732ba56f112
ngodi <albertngodi@gmail.com>
2020-02-18 21:24:59 +0100

    projects description updated

--- src/Components/Projects/projectsData.js
+++ src/Components/Projects/projectsData.js
@@ -3,7 +3,7 @@ export const projectList = [
     type: "front",
     title: "Newsweek Magazine Clone",
     preview: "./images/newsweek.png",
-    desc:" Built by a team of two, where both members contributed fully in this project from conception, design, implementation, testing to deployment. The ruby on rails full-stack framework was used in  building this project.",
+    desc:"  Utilized HTML, CSS and BOOTSTRAP to develop a clone of the Newsweek magazine website as part of my ‘Microverse’ training in responsive design.",
     tech: ['HTML5', 'CSS3', 'BOOTSTRAP'],
     code: "https://github.com/ngodi/NEWSWEEK",
     demo: "https://raw.githack.com/ngodi/NEWSWEEK/development/index.html"
@@ -12,7 +12,7 @@ export const projectList = [
     type: "full",
     title: "Ufriends Social Network",
     preview: "./images/ufriends.png",
-    desc:"   Utilized HTML, CSS and BOOTSTRAP to develop a clone of the Newsweek magazine website as part of my ‘Microverse’ training in responsive design.",
+    desc:"Built by a team of two, where both members contributed fully in this project from conception, design, implementation, testing to deployment. The ruby on rails full-stack framework was used in  building this project.",
     tech: ['RUBY ON RAILS','HTML5', 'CSS3', 'BOOTSTRAP'],
     code: "https://github.com/diegoarvz4/building_facebook",
     demo: "https://intense-refuge-98385.herokuapp.com/"
@@ -23,8 +23,8 @@ export const projectList = [
     preview: "./images/k_orsi_shop.png",
     desc:"   Utilized React to develop the frontend of  ‘K-ORSI SHOP’, an e-commerce website. Built project from conception,  implementation to deployment.",
     tech: ['HTML', 'CSS', 'REACT.JS', 'JAVASCRIPT'],
-    code: "https://github.com/ngodi/JQuery_Drawing_App",
-    demo: "https://drawing-app.netlify.com/"
+    code: "https://github.com/ngodi/k-orsi-shop",
+    demo: "https://korsi.netlify.com"
     }
 ];
 
