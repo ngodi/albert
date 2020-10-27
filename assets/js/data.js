ea298d9a2e42a5a5d8c5c9179af65e1d9e535404
ngodi <albertngdi@gmail.com>
2020-10-27 06:41:15 +0100

    portfolio update

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,56 +1,59 @@
-export const projectList = [
+const about_text = "A talented software developer with experience providing web solutions using the latest technology trends. In the frontend, I work with Javascript, React.js, HTML/CSS, bootstrap, SASS, while in the backend I work with Ruby on Rails, Node.js, and Mysql. I also have excellent communication and collaboration skills.";
+
+document.querySelector(".about__text").innerHTML = about_text;
+
+const work = [
     {
-    type: "front",
     title: "BATTLESHIP",
-    preview: "./images/battleship.png",
+    preview: "./assets/img/battleship.png",
     desc:"Battleship is a game played on the browser between a user and the computer. The game ends when all the ships on a given gameboard have all been hit; they are said to be sunk, the opponent is then declared winner.",
     tech: ['HTML', 'CSS', 'JAVASCRIPT', 'JEST'],
     code: "https://github.com/ngodi/battleship-game",
     demo: "https://battleship-js.netlify.com/"
     },
     {
-    type: "full",
     title: "Ufriends Social Network",
-    preview: "./images/ufriends.png",
+    preview: "./assets/img/ufriends.png",
     desc:"Built by a team of two, where both members contributed fully in this project from conception, design, implementation, testing to deployment. The ruby on rails full-stack framework was used in  building this project.",
     tech: ['RUBY ON RAILS','HTML5', 'CSS3', 'BOOTSTRAP'],
     code: "https://github.com/diegoarvz4/building_facebook",
     demo: "https://intense-refuge-98385.herokuapp.com/"
     },
     {
-    type: "full",
     title: "BookTracker CMS",
-    preview: "./images/booktracker.png",
+    preview: "./assets/img/booktracker.png",
     desc:" BookTracker CMS is used to store and keep track of books read by a particular user. This enables the user to easily visualise all the books read as well as the content covered and current chapter being read.",
     tech: ['REDUX', 'REACT.JS'],
     code: "https://github.com/ngodi/redux-bookstore",
     demo: "https://booktracker.netlify.com/"
     },
     {
-    type: "full",
-    title: "K-ORSI SHOP",
-    preview: "./images/korsi.png",
-    desc:"This project is the front-end of a furniture shop; 'K-ORSI SHOP'. The front-end showcases different furniture supplies they offer and allows users to place orders online.",
-    tech: ['REACT.JS','HTML', 'CSS'],
-    code: "https://github.com/ngodi/k-orsi-shop",
-    demo: "https://korsi.netlify.com/"
-    },
-    {
-    type: "full",
     title: "Drawing App",
-    preview: "./images/drawing.png",
+    preview: "./assets/img/drawing.png",
     desc:"Web drawing app using HTML Canvas, jQuery and jQueryUI. Very interactive and good for kids learning to draw.",
     tech: ['JQuery', 'JQUERYUI','HTML5 CANVAS', 'CSS'],
     code: "https://github.com/ngodi/JQuery_Drawing_App",
     demo: "https://drawing-app.netlify.com/"
-    },
-    {
-    type: "front",
-    title: "CSS GRID FRAMEWORK",
-    preview: "./images/grid.png",
-    desc:"The css grid framework project consists of a custom css layout framework, as part a practice project. The framework is then used the Odin webpage clone which is available here: https://www.theodinproject.com/.",
-    tech: ['CSS', 'CSS FLEXBOX', 'CSS FLOATS'],
-    code: "https://github.com/ngodi/css-grid-framework",
-    demo: "https://raw.githack.com/ngodi/css-grid-framework/development/index.html"
     }
 ];
+
+for (let item of work) {
+ let tec = item.tech.map(item => `
+    <span>${item}</span>
+ `)
+  document.querySelector(".work__container")
+   .insertAdjacentHTML('beforeend',
+  `<div class="work__img">
+      <div class="work__title">${item.title}</div>
+      <div class="tech__stack">${tec}</div>
+      <img src="${item.preview}" alt="" width="100%">
+      <p class="work__description">${item.desc}</p>
+      <div class="work__buttons">
+        <a target="_blank" class="left" href=${item.code}><i class='bx bxl-github'></i>source code</a>
+        <a target="_blank" class="right" href=${item.demo}><i class='bx bxl-internet-explorer'></i>live demo</a>
+      </div>
+   </div>
+
+   `
+  )
+}
