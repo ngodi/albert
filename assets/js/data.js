15e3611f07462aafe73fadcd47a286ff638174e7
Ngodi Albert <albertngodi@gmail.com>
2020-10-28 17:38:24 +0100

    form fields validation

---
+++ assets/js/data.js
@@ -0,0 +1,59 @@
+const about_text = "A talented software developer with experience providing web solutions using the latest technology trends. In the frontend, I work with Javascript, React.js, HTML/CSS, bootstrap, SASS, while in the backend I work with Ruby on Rails, Node.js, and Mysql. I also have excellent communication and collaboration skills.";
+
+document.querySelector(".about__text").innerHTML = about_text;
+
+const work = [
+    {
+    title: "BATTLESHIP",
+    preview: "./assets/img/battleship.png",
+    desc:"Battleship is a game played on the browser between a user and the computer. The game ends when all the ships on a given gameboard have all been hit; they are said to be sunk, the opponent is then declared winner.",
+    tech: ['HTML', 'CSS', 'JAVASCRIPT', 'JEST'],
+    code: "https://github.com/ngodi/battleship-game",
+    demo: "https://battleship-js.netlify.com/"
+    },
+    {
+    title: "Ufriends Social Network",
+    preview: "./assets/img/ufriends.png",
+    desc:"Built by a team of two, where both members contributed fully in this project from conception, design, implementation, testing to deployment. The ruby on rails full-stack framework was used in  building this project.",
+    tech: ['RUBY ON RAILS','HTML5', 'CSS3', 'BOOTSTRAP'],
+    code: "https://github.com/diegoarvz4/building_facebook",
+    demo: "https://intense-refuge-98385.herokuapp.com/"
+    },
+    {
+    title: "BookTracker CMS",
+    preview: "./assets/img/booktracker.png",
+    desc:" BookTracker CMS is used to store and keep track of books read by a particular user. This enables the user to easily visualise all the books read as well as the content covered and current chapter being read.",
+    tech: ['REDUX', 'REACT.JS'],
+    code: "https://github.com/ngodi/redux-bookstore",
+    demo: "https://booktracker.netlify.com/"
+    },
+    {
+    title: "Drawing App",
+    preview: "./assets/img/drawing.png",
+    desc:"Web drawing app using HTML Canvas, jQuery and jQueryUI. Very interactive and good for kids learning to draw.",
+    tech: ['JQuery', 'JQUERYUI','HTML5 CANVAS', 'CSS'],
+    code: "https://github.com/ngodi/JQuery_Drawing_App",
+    demo: "https://drawing-app.netlify.com/"
+    }
+];
+
+for (let item of work) {
+ let tec = item.tech.map(item => `
+    <span>${item}</span>
+ `)
+  document.querySelector(".work__container")
+   .insertAdjacentHTML('beforeend',
+  `<div class="work__img">
+      <div class="work__title">${item.title}</div>
+      <div class="tech__stack">${tec}</div>
+      <img src="${item.preview}" alt="" width="100%">
+      <p class="work__description">${item.desc}</p>
+      <div class="work__buttons">
+        <a target="_blank" class="left" href=${item.code}><i class='bx bxl-github'></i>source code</a>
+        <a target="_blank" class="right" href=${item.demo}><i class='bx bxl-internet-explorer'></i>live demo</a>
+      </div>
+   </div>
+
+   `
+  )
+}
---
+++ assets/js/main.js
@@ -0,0 +1,89 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+          toggle.addEventListener('click', () => {
+              nav.classList.toggle('show')
+          })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu');
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home__title', {})
+sr.reveal('.button', {delay: 200})
+sr.reveal('.home__img', {delay: 400})
+sr.reveal('.home__social-icon', {interval: 200})
+
+sr.reveal('.about__img', {})
+sr.reveal('.about__subtitle', {delay: 200})
+sr.reveal('.about__text', {delay: 400})
+
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__text', {delay: 200})
+sr.reveal('.skills__data', {interval: 200})
+sr.reveal('.skills__img', {delay: 400})
+
+sr.reveal('.work__img', {interval: 200})
+
+sr.reveal('.contact__input', {interval: 200})
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    name.addEventListener('focus', () => {
+        clearFields(name);
+    })
+    email.addEventListener('focus', () => {
+        clearFields(email);
+    })
+    message.addEventListener('focus', () => {
+        clearFields(message);
+    })
+
+document.getElementById("buttonClick").addEventListener("click", () => {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    checkField(name);
+    checkField(email);
+    checkField(message);
+   
+
+})
+
+const checkField = input => {
+    if (!input.value) {
+        input.classList.add("input__error");
+        input.value= ` ${input.id} field must not be empty`;
+        document.querySelector(".error").innerHTML="Please check the required fields above";
+    }
+}
+
+const clearFields = (input) => {
+    input.value = '';
+    input.classList.remove("input__error");
+    document.querySelector(".error").innerHTML = "";
+}
